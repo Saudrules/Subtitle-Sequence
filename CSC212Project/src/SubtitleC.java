7d8990c57474c26805b2c5bdded9538639f521e8
Ubeast <33600445+Ubeast@users.noreply.github.com>
2017-11-12 20:01:01 +0300

    Class Implement init

---
+++ CSC212Project/src/SubtitleC.java
@@ -0,0 +1,40 @@
+
+public class SubtitleC implements Subtitle {
+
+	@Override
+	public Time getStartTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Time getEndTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStartTime(Time startTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEndTime(Time endTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setText(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ CSC212Project/src/TimeC.java
@@ -0,0 +1,52 @@
+
+public class TimeC implements Time {
+
+	@Override
+	public int getHH() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMM() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSS() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMS() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setHH(int hh) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMM(int mm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSS(int ss) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMS(int ms) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
