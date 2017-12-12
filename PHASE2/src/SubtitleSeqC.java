ecf1884b44c82ce4890f6b23ba7bb4581e8bcfcc
Ubeast <33600445+Ubeast@users.noreply.github.com>
2017-12-12 15:10:04 +0300

    - public List<Subtitle> getSubtitles(Time startTime, Time endTime) .

--- PHASE2/src/SubtitleSeqC.java
+++ PHASE2/src/SubtitleSeqC.java
@@ -1,23 +1,21 @@
 public class SubtitleSeqC implements SubtitleSeq {
-	
-	
-	private SortedMap<TimeInterval,Subtitle> sortedSubs = new SortedBST<TimeInterval,Subtitle>() ;
-	
-	
+
+	private SortedMap<TimeInterval, Subtitle> sortedSubs = new SortedBST<TimeInterval, Subtitle>();
+
 	@Override
 	public void addSubtitle(Subtitle st) {
-		if(sortedSubs.empty())
+		if (sortedSubs.empty())
 			sortedSubs.insert(new TimeInterval(st), st);
-		
-		else if(!isOverlapping(st)) {
+
+		else if (!isOverlapping(st)) {
 			Subtitle tmpSub;
 			TimeInterval tmpTime = new TimeInterval(st);
 			TimeInterval curTime;
 
 			sortedSubs.findFirst();
-			while(!sortedSubs.last()) {
+			while (!sortedSubs.last()) {
 				curTime = new TimeInterval(sortedSubs.retrieve().second);
-				if(tmpTime.compareTo(curTime) < 0) {
+				if (tmpTime.compareTo(curTime) < 0) {
 					tmpSub = sortedSubs.retrieve().second;
 					sortedSubs.update(st);
 					sortedSubs.insert(new TimeInterval(tmpSub), tmpSub);
@@ -26,7 +24,7 @@ public class SubtitleSeqC implements SubtitleSeq {
 				sortedSubs.findNext();
 			}
 			curTime = new TimeInterval(sortedSubs.retrieve().second);
-			if(tmpTime.compareTo(curTime) < 0) {
+			if (tmpTime.compareTo(curTime) < 0) {
 				tmpSub = sortedSubs.retrieve().second;
 				sortedSubs.update(st);
 				sortedSubs.insert(new TimeInterval(tmpSub), tmpSub);
@@ -36,141 +34,151 @@ public class SubtitleSeqC implements SubtitleSeq {
 			sortedSubs.insert(new TimeInterval(st), st);
 			sortedSubs.findNext();
 		}
-		
-		else 
+
+		else
 			return;
 	}
 
 	@Override
-	public List<Subtitle> getSubtitles(){
+	public List<Subtitle> getSubtitles() {
 		List<Subtitle> tmp = new LinkedList<Subtitle>();
-		
+
 		sortedSubs.findFirst();
-		while(!sortedSubs.last()) {
+		while (!sortedSubs.last()) {
 			addSubtitle(sortedSubs.retrieve().second, tmp);
 			sortedSubs.findNext();
 		}
 		addSubtitle(sortedSubs.retrieve().second, tmp);
-		
+
 		return tmp;
 	}
-	
+
 	@Override
 	public Subtitle getSubtitle(Time time) {
-		if(this.sortedSubs.empty())
+		if (this.sortedSubs.empty())
 			return null;
 		sortedSubs.findFirst();
-		while(!sortedSubs.last()){
-			if(toMS(sortedSubs.retrieve().second.getStartTime()) <= toMS(time) &&
-					toMS(sortedSubs.retrieve().second.getEndTime()) >= toMS(time))
+		while (!sortedSubs.last()) {
+			if (toMS(sortedSubs.retrieve().second.getStartTime()) <= toMS(time)
+					&& toMS(sortedSubs.retrieve().second.getEndTime()) >= toMS(time))
 				return sortedSubs.retrieve().second;
 			sortedSubs.findNext();
 		}
-		if(toMS(sortedSubs.retrieve().second.getStartTime()) <= toMS(time) &&
-				toMS(sortedSubs.retrieve().second.getEndTime()) >= toMS(time))
+		if (toMS(sortedSubs.retrieve().second.getStartTime()) <= toMS(time)
+				&& toMS(sortedSubs.retrieve().second.getEndTime()) >= toMS(time))
 			return sortedSubs.retrieve().second;
-		
+
 		return null;
 	}
-	
+
 	@Override
 	public int nbNodesInSearchPath(Time time) {
 		int cpt = 0;
 		sortedSubs.findFirst();
-		while(!sortedSubs.last()){
+		while (!sortedSubs.last()) {
 			cpt++;
-			if(toMS(sortedSubs.retrieve().second.getStartTime()) <= toMS(time) &&
-					toMS(sortedSubs.retrieve().second.getEndTime()) >= toMS(time))
-						return cpt;
+			if (toMS(sortedSubs.retrieve().second.getStartTime()) <= toMS(time)
+					&& toMS(sortedSubs.retrieve().second.getEndTime()) >= toMS(time))
+				return cpt;
 			sortedSubs.findNext();
 		}
-		
+
 		cpt++;
 		return cpt;
 	}
-	
+
 	@Override
 	public List<Subtitle> getSubtitles(Time startTime, Time endTime) {
-		
+		List<Subtitle> tmpList = new LinkedList<Subtitle>();
+
+		sortedSubs.findFirst();
+		while (!sortedSubs.last()) {
+			if (toMS(sortedSubs.retrieve().second.getEndTime()) >= toMS(startTime)
+					&& (toMS(sortedSubs.retrieve().second.getStartTime()) <= toMS(endTime)))
+				tmpList.insert(sortedSubs.retrieve().second);
+
+			sortedSubs.findNext();
+		}
+		if (toMS(sortedSubs.retrieve().second.getEndTime()) >= toMS(startTime)
+				&& (toMS(sortedSubs.retrieve().second.getStartTime()) <= toMS(endTime)))
+			tmpList.insert(sortedSubs.retrieve().second);
+
+		return tmpList;
 	}
 
-	
 	@Override
 	public void shift(int offset) {
-		
-	}
-		
-	private static int toMS(Time t){
-			return (t.getHH()*3600000 + t.getMM()*60000 + t.getSS()*1000 + t.getMS()) ;
-	}
-		
-		private static Time toTime(int ms){
-			int RealTimeHH = ((ms/3600000)%24) ;
-			int RealTimeMM = ((ms/60000)%60) ;
-			int RealTimeSS = ((ms/1000)%60) ;
-			int RealTimeMS = ms%1000 ;
-			Time R = new TimeC();
-			R.setHH(RealTimeHH);
-			R.setMM(RealTimeMM);
-			R.setSS(RealTimeSS);
-			R.setMS(RealTimeMS);
-			return R;
-		}
-		//Try compareTo later
-		private boolean isOverlapping(Subtitle st) {
-			sortedSubs.findFirst();
-			while(!sortedSubs.last()) {
-			Pair<TimeInterval, Subtitle> sub = sortedSubs.retrieve();
-			Time end = sub.second.getEndTime();
-			Time start = sub.second.getStartTime();
-				if(toMS(end)>=toMS(st.getStartTime())&&
-						(toMS(start)<=toMS(st.getEndTime())))
-					return true;
-				sortedSubs.findNext();
-			}
-			Pair<TimeInterval, Subtitle> sub = sortedSubs.retrieve();
-			Time end = sub.second.getEndTime();
-			Time start = sub.second.getStartTime();
-			if(toMS(end)>=toMS(st.getStartTime())&&
-					(toMS(start)<=toMS(st.getEndTime())))
-				return true;
-			return false;
-		}
-		
-		private void addSubtitle(Subtitle st, List<Subtitle> listOfSubs) {
-			
-			if(listOfSubs.empty())
-				listOfSubs.insert(st);
-				else if(!isOverlapping(st)) {
-				Subtitle tmpSub;
-				int tmpTime = toMS(st.getStartTime());
-				int curTime;
 
-				listOfSubs.findFirst();
-				while(!listOfSubs.last()) {
-					curTime = toMS(listOfSubs.retrieve().getStartTime());
-					if(tmpTime<curTime) {
-						tmpSub = listOfSubs.retrieve();
-						listOfSubs.update(st);
-						listOfSubs.insert(tmpSub);
-						return;
-					}
-					listOfSubs.findNext();
-				}
+	}
+
+	private static int toMS(Time t) {
+		return (t.getHH() * 3600000 + t.getMM() * 60000 + t.getSS() * 1000 + t.getMS());
+	}
+
+	private static Time toTime(int ms) {
+		int RealTimeHH = ((ms / 3600000) % 24);
+		int RealTimeMM = ((ms / 60000) % 60);
+		int RealTimeSS = ((ms / 1000) % 60);
+		int RealTimeMS = ms % 1000;
+		Time R = new TimeC();
+		R.setHH(RealTimeHH);
+		R.setMM(RealTimeMM);
+		R.setSS(RealTimeSS);
+		R.setMS(RealTimeMS);
+		return R;
+	}
+
+	// Try compareTo later
+	private boolean isOverlapping(Subtitle st) {
+		sortedSubs.findFirst();
+		while (!sortedSubs.last()) {
+			Pair<TimeInterval, Subtitle> sub = sortedSubs.retrieve();
+			Time end = sub.second.getEndTime();
+			Time start = sub.second.getStartTime();
+			if (toMS(end) >= toMS(st.getStartTime()) && (toMS(start) <= toMS(st.getEndTime())))
+				return true;
+			sortedSubs.findNext();
+		}
+		Pair<TimeInterval, Subtitle> sub = sortedSubs.retrieve();
+		Time end = sub.second.getEndTime();
+		Time start = sub.second.getStartTime();
+		if (toMS(end) >= toMS(st.getStartTime()) && (toMS(start) <= toMS(st.getEndTime())))
+			return true;
+		return false;
+	}
+
+	private void addSubtitle(Subtitle st, List<Subtitle> listOfSubs) {
+
+		if (listOfSubs.empty())
+			listOfSubs.insert(st);
+		else if (!isOverlapping(st)) {
+			Subtitle tmpSub;
+			int tmpTime = toMS(st.getStartTime());
+			int curTime;
+
+			listOfSubs.findFirst();
+			while (!listOfSubs.last()) {
 				curTime = toMS(listOfSubs.retrieve().getStartTime());
-				if(tmpTime<curTime) {
+				if (tmpTime < curTime) {
 					tmpSub = listOfSubs.retrieve();
 					listOfSubs.update(st);
 					listOfSubs.insert(tmpSub);
 					return;
 				}
-
-				listOfSubs.insert(st);
 				listOfSubs.findNext();
-				}
-				else 
-					return;
-		}
-		
+			}
+			curTime = toMS(listOfSubs.retrieve().getStartTime());
+			if (tmpTime < curTime) {
+				tmpSub = listOfSubs.retrieve();
+				listOfSubs.update(st);
+				listOfSubs.insert(tmpSub);
+				return;
+			}
+
+			listOfSubs.insert(st);
+			listOfSubs.findNext();
+		} else
+			return;
+	}
 
 }
